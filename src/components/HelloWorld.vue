09612ecf505c19c81a3f17e4be60496cc868ca30
Mohamed Mostafa <mo.mostafa@nu.edu.eg>
2019-05-30 01:35:20 +0200

    final

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,7 +17,8 @@
         </ul>
       </div>
     </nav>
-    <table class="table sc-table rounded container">
+
+    <table v-if="auth" class="table sc-table rounded container">
       <caption>
         Spring 19 class schedule
         <p>
@@ -59,8 +60,11 @@
         </tr>
       </tbody>
     </table>
-    <div class="container">
+    <div v-if="auth" class="container">
       <button class="btn btn-primary float-right" @click="submitFinal()">Submit</button>
+    </div>
+    <div v-if="!auth">
+      <h1>You don`t have access right now to the schedule, please log in later in your appointed time.</h1>
     </div>
   </div>
 </template>
@@ -69,161 +73,91 @@
 import Vue from "vue";
 import VueToast from "vue-toast-notification";
 import "vue-toast-notification/dist/index.css";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import Axios from "axios";
 Vue.use(VueToast);
+Vue.use(VueAxios, axios);
 export default {
-  created() {
-    this.receiveNotificatoin();
-  },
   data() {
     return {
       UserName: "Mohamed Mostafa Mostafa",
       days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"],
-      schedule: [
-        [
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: false, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ]
-        ],
-        [
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ]
-        ],
-        [
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ]
-        ],
-        [
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ]
-        ],
-        [
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ],
-          [
-            { name: "ECEN 203", id: "12", valid: true, class: "inactiveClass" },
-            { name: "CSES 203", id: "13", valid: true, class: "inactiveClass" }
-          ]
-        ]
-      ]
+      schedule: [],
+      courses: [],
+      auth: false,
+      student: ""
     };
+  },
+  mounted() {
+    console.clear();
+    this.student = localStorage.student;
+    this.authenticateTime();
+    this.getSchedule();
+    this.receiveNotificatoin();
+    console.clear();
+  },
+  watch: {
+    student(newStudnet) {
+      localStorage.student = newStudnet;
+    }
   },
   methods: {
     receiveNotificatoin() {
-      Vue.$toast.error(
-        "Some students are asking to join CS201-4 that you are enrolled in and it`s full. so, if you can recreate your schedule and register in another class would be appreciated.",
-        { duration: 20000, position: "top-left" }
-      );
+      if (this.auth) {
+        Vue.$toast.info(
+          "Some students are asking to join CS201-4 that you are enrolled in and it`s full. so, if you can recreate your schedule and register in another class would be appreciated.",
+          { duration: 20000, position: "top-left" }
+        );
+      }
+    },
+    authenticateTime() {
+      Axios.get(
+        "http://localhost:5000/api/v1/auth/" + this.student,
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*"
+          }
+        },
+        { crossdomain: true }
+      )
+        .then(res => {
+          console.log("res", res);
+          this.auth = res.data.success;
+        })
+        .catch(err => {
+        });
+    },
+    getSchedule() {
+      Axios.get(
+        "http://localhost:5000/api/v1/schedule",
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*"
+          }
+        },
+        { crossdomain: true }
+      )
+        .then(res => {
+          console.log("res", res);
+          this.schedule = res.data.data;
+        })
+        .catch(err => {
+        });
+    },
+    arrayRemove(arr, value) {
+      return arr.filter(function(ele) {
+        return ele != value;
+      });
     },
     selectClass(day, slot, lecture) {
       if (this.schedule[day][slot][lecture].class == "inactiveClass") {
         this.schedule[day][slot][lecture].class = "activeClass";
+        this.courses.push(this.schedule[day][slot][lecture].id);
       } else if (this.schedule[day][slot][lecture].class == "activeClass") {
         this.schedule[day][slot][lecture].class = "inactiveClass";
+        this.courses.pop(this.schedule[day][slot][lecture].id);
+        this.arrayRemove(this.courses, this.schedule[day][slot][lecture].id);
       }
       console.log("this", day, slot, lecture);
       console.log("day", this.schedule[day][slot][lecture]);
@@ -234,14 +168,36 @@ export default {
       Vue.$toast.success("Request Sent Succefully!", { position: "top-left" });
     },
     submitFinal() {
-      Vue.$toast.success("Submitted successfully!", { position: "top-left" });
-      this.schedule.forEach(day => {
-        day.forEach(slot => {
-          slot.forEach(lecture => {
-            lecture.class = "inactiveClass";
-          });
+      if (this.student == "" || this.student == undefined) {
+        Vue.$toast.error("Student Not Found", { position: "top-left" });
+      } else if (this.courses.length < 9) {
+        Vue.$toast.error("please select at least 9 slots", {
+          position: "top-left"
         });
-      });
+      } else {
+        if (confirm("Confirm Submissoin ?")) {
+          axios
+            .post("http://localhost:5000/api/v1/courses", {
+              studentID: this.student,
+              courses: this.courses
+            })
+            .then(res => {
+              console.log("res", res);
+            })
+            .catch(err => {
+            });
+          Vue.$toast.success("Submitted successfully!", {
+            position: "top-left"
+          });
+          this.schedule.forEach(day => {
+            day.forEach(slot => {
+              slot.forEach(lecture => {
+                lecture.class = "inactiveClass";
+              });
+            });
+          });
+        }
+      }
     }
   }
 };
